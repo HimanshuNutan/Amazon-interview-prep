d81b9e1a4a1bbf6a93f7e89ef5582842375593f2
HimanshuNutan <57312592+HimanshuNutan@users.noreply.github.com>
2022-06-23 12:47:04 +0530

    Add files via upload

--- TransactionLogs.java
+++ TransactionLogs.java
@@ -4,15 +4,25 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
+/*
+Input -> Each input is a transaction where format is
+userid1 userid2 transaction_amount
 
+and a threshold value
+Find all the users making transactions more than threshold value
+
+if transaction is done to same user, it's considered as 1 transaction and not 2
+
+ */
 public class TransactionLogs {
     public static void main(String[] args) {
         List<String> logs = new ArrayList<>();
+        int threshold = 2;
         logs.add("88 99 200");
         logs.add("88 99 300");
         logs.add("99 32 100");
         logs.add("12 12 15");
-        System.out.println(processLogs(logs,2));
+        System.out.println(processLogs(logs,threshold));
     }
 
     public static List<String> processLogs(List<String> logs,int thrashold){
