6e5fc3deffadd767350e58d798e64b2df0acc46a
HimanshuNutan <57312592+HimanshuNutan@users.noreply.github.com>
2022-06-29 11:08:20 +0530

    Add files via upload

---
+++ Practice1_Amazon/Fraction.java
@@ -0,0 +1,39 @@
+package Practice1_Amazon;
+
+import java.util.HashMap;
+
+public class Fraction {
+    public static void main(String[] args) {
+        int n=45,d=22,rem;
+
+        int fix = n/d;
+        int point = n%d;
+        if(n%d!=0){
+        double actual = (double)n/(double)d;
+        double diff = actual-(double)fix;
+        String s = ""+diff;
+        s = s.substring(2,s.length());
+        HashMap<Character,Integer> map = new HashMap<>();
+        int index=0;boolean flag = false;
+
+        for(int i=0;i<s.length();i++){
+            if(!map.containsKey(s.charAt(i))) {
+                map.put(s.charAt(i), i);
+            }else{
+                flag = true;
+                index=i;break;
+            }
+        }
+        String finalString="";
+        if(flag)
+         finalString = fix+".("+s.substring(0,index)+")";
+        else
+            finalString = fix+"."+s;
+            System.out.println(finalString);
+
+        }else{
+            System.out.println(fix);
+        }
+
+    }
+}
---
+++ Practice1_Amazon/MissingNumber.java
@@ -0,0 +1,13 @@
+package Practice1_Amazon;
+
+import java.util.Arrays;
+
+public class MissingNumber {
+    public static void main(String[] args) {
+        int ar[] = {8,5,1,2,9,4,6,7};
+        int sum = Arrays.stream(ar).sum();
+        int n =ar.length+1;
+        int expSum = (n*(n+1))/2;
+        System.out.println(expSum-sum);
+    }
+}
